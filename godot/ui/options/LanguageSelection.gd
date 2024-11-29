18931cb9105d3532407b0d2af11c53e249f61cc9
CaesiumFox <avdeev20020904@gmail.com>
2024-11-29 21:56:12 +0300

    Add language buttons for mouse

--- godot/ui/options/LanguageSelection.gd
+++ godot/ui/options/LanguageSelection.gd
@@ -13,19 +13,23 @@ var lang_change: Dictionary = {
 
 func _ready() -> void:
     current = TranslationServer.get_locale()
-    text = "\u2039 " + tr("O_MY_NAME") + " \u203a"
 
 func update() -> void:
     SaveManager.settings.language = current
-    text = "\u2039 " + tr("O_MY_NAME") + " \u203a"
 
 func _process(_delta: float) -> void:
     if has_focus():
         if Input.is_action_just_pressed("ui_left"):
-            current = lang_change[current]["left"]
-            changed.emit(current)
-            update()
+            _on_left_pressed()
         if Input.is_action_just_pressed("ui_right"):
-            current = lang_change[current]["right"]
-            changed.emit(current)
-            update()
+            _on_right_pressed()
+
+func _on_left_pressed() -> void:
+    current = lang_change[current]["left"]
+    changed.emit(current)
+    update()
+
+func _on_right_pressed() -> void:
+    current = lang_change[current]["right"]
+    changed.emit(current)
+    update()
