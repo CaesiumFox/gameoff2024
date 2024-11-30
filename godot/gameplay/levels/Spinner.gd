89a63696b077549780476b21b37998f2bb40732e
CaesiumFox <avdeev20020904@gmail.com>
2024-12-01 00:37:17 +0300

    Add interpolate

--- godot/gameplay/levels/Spinner.gd
+++ godot/gameplay/levels/Spinner.gd
@@ -1,4 +1,5 @@
 extends TileMapLayer
+class_name Spinner
 
 @export var count: int = 0
 @export var total_cells: Array[Vector2i] = []
@@ -10,6 +11,16 @@ extends TileMapLayer
 
 var frame: int = 0
 
+static func interpolate(array: Array[Vector2i], from: Vector2i, to: Vector2i) -> void:
+    if abs(from.x - to.x) == 0:
+        # vertical
+        for y in range(from.y, to.y, 1 if to.y >= from.y else -1):
+            array.append(Vector2i(from.x, y))
+    elif abs(from.y - to.y) == 0:
+        # horizontal
+        for x in range(from.x, to.x, 1 if to.x >= from.x else -1):
+            array.append(Vector2i(x, from.y))
+
 func _ready() -> void:
     for child in get_children():
         if child is Timer:
@@ -20,12 +31,12 @@ func _on_timer_timeout() -> void:
     if total_cells.size() == 0:
         return
     clear()
-    frame += delta_frame
-    frame %= total_cells.size()
     var cells: Array[Vector2i] = []
     var f := 0
     for i in range(count):
-        f += delta_cells[i % delta_cells.size()]
         cells.append(total_cells[(frame + f) % total_cells.size()])
-    set_cells_terrain_connect(cells, 2, 0)
+        f += delta_cells[i % delta_cells.size()]
+    frame += delta_frame
+    frame %= total_cells.size()
+    set_cells_terrain_connect(cells, terrain_set, terrain)
     
